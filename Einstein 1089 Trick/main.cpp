086da8c3de9abd94a61d6b8f1839f6bf7e714c3c
Jonathan Martin <jonmartin721@gmail.com>
2017-05-05 16:59:06 -0500

    Began text display.

--- Einstein 1089 Trick/main.cpp
+++ Einstein 1089 Trick/main.cpp
@@ -1,11 +1,35 @@
 // Einstein 1089 Trick.cpp : Defines the entry point for the console application.
 //
 
+//All credit for the idea and documents given to the material at 
+//http://www.cse.msu.edu/~cse231/PracticeOfComputingUsingPython/
+
 #include "stdafx.h"
+#include <iostream>
 
 
 int main()
 {
+	std::cout << "Einstein took great delight in baffling friends with the trick about to be shown to you.\n\n" ;
+	std::cout << "Please enter a 3-digit number, but the first and last digits must be different by at least 2." ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
 
